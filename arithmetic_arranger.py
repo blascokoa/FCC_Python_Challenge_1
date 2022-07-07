f8f614c99776754fdbb92f8b013b2e058a3e430b
Blasco <blascokoa1@gmail.com>
2022-07-07 13:26:15 +0200

    refactor: clean code

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -2,36 +2,38 @@ import re
 
 
 def arithmetic_arranger(problems, solve=False):
+    # Solution requires:
+    #  2 blank spaces from the longest one (if the longest doesn't have the math symbol)
+    #  4 black spaces from one problem to another
+    #  same number of "-" as the longest one + 2
+
     if len(problems) > 5:
         return 'Error: Too many problems.'
     numbers_split = [re.split(" [+-] ", problem) for problem in problems]
     symbol_split = [re.split("[0-9]", problem) for problem in problems]
     symbol_split_clean = [list(filter(None, symbol))[0] for symbol in symbol_split]
 
-    # TODO Solution requires:
-    #  2 blank spaces from the longest one (if the longest doesnt got the math symbol
-    #  4 black spaces from one problem to another
-    #  same number of "-" as the longest one + 2
     counter = 0
     divider = []
     solutions = []
+
     for number in numbers_split:
-        # Position 0 goes up
-        if solve:
-            if symbol_split_clean[counter][1] == '+':
-                solution = int(number[0]) + int(number[1])
-            elif symbol_split_clean[counter][1] == '-':
-                solution = int(number[0]) - int(number[1])
         if symbol_split_clean[counter][1] != '+' and symbol_split_clean[counter][1] != '-':
             return "Error: Operator must be '+' or '-'."
         if len(number[0]) > 4 or len(number[1]) > 4:
             return "Error: Numbers cannot be more than four digits."
         if not number[0].isdigit() or not number[1].isdigit():
             return "Error: Numbers must only contain digits."
+
+        if solve:
+            if symbol_split_clean[counter][1] == '+':
+                solution = int(number[0]) + int(number[1])
+            elif symbol_split_clean[counter][1] == '-':
+                solution = int(number[0]) - int(number[1])
+
         if len(number[0]) > len(number[1]):
             number[0] = "  " + number[0]
             number[1] = symbol_split_clean[counter][1] + " " * (len(number[0])-len(number[1]) -1) + number[1]
-
             divider.append("-" * (len(number[0])))
         elif len(number[0]) == len(number[1]):
             number[0] = "  " + number[0]
@@ -43,6 +45,7 @@ def arithmetic_arranger(problems, solve=False):
             number[0] = " " * (len(number[1])) + number[0]
             number[1] = symbol_split_clean[counter][1] + " " + number[1]
             divider.append("-" * (len(number[0])))
+
         counter += 1
         if solve:
             solutions.append(str(" " * (len(str(number[0])) - len(str(solution)))) + str(solution))
@@ -53,12 +56,12 @@ def arithmetic_arranger(problems, solve=False):
             if solve:
                 solutions.append(" " * 4)
 
-    new_list = ["", "", ""]
+    solution_list = ["", "", ""]
     for number in numbers_split:
-        new_list[0] += number[0]
-        new_list[1] += number[1]
+        solution_list[0] += number[0]
+        solution_list[1] += number[1]
 
-    result = new_list[0] + "\n" + new_list[1] + "\n" + "".join(divider)
+    result = solution_list[0] + "\n" + solution_list[1] + "\n" + "".join(divider)
     if solve:
         result += "\n" + "".join(solutions)
 
